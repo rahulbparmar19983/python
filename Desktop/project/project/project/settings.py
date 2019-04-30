9506ffd31a1e8f7ce07bf4b8357cbf23593ab13b
rahulbparmar19983 <42406096+rahulbparmar19983@users.noreply.github.com>
2019-04-30 10:08:20 +0530

    Update settings.py

--- Desktop/project/project/project/settings.py
+++ Desktop/project/project/project/settings.py
@@ -129,8 +129,8 @@ DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rahulbparmar19983@gmail.com'
-EMAIL_HOST_PASSWORD = 'rahulsahil'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
